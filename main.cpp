2f8eda6be6e418b46c0f1d3d81dacf41e06792fd
AKHMAD SAIFUL ANAM <45529658+akhmadaan@users.noreply.github.com>
2019-01-06 02:16:56 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+const float phi = 3.14;
+const char nama [] = "Akhmad Saiful Anam";
+//float luas;
+
+
+int main()
+{
+    system("color d");
+    printf("%f\n",phi);
+    float luas = 7*7*phi;
+
+    //luas = 7*7*phi;
+    printf("%f\n",luas);
+    printf("%s\n",nama);
+ getch();
+    return 0;
+}
